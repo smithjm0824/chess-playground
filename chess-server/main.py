6a35f976d0676e78b959228e4adc9ef4beb00c30
Josh Smith <jsmith@joshs-mbp.attlocal.net>
2020-06-10 00:28:19 -0500

    Added a vanilla minimax player

--- chess-server/main.py
+++ chess-server/main.py
@@ -1,6 +1,7 @@
 import chess
-import players
+from players import Player
 import json
+import time
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
@@ -13,7 +14,8 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 def hello_world():
     game = Game()
-    game.play_game(players.random_player, players.random_player)
+    players = Player()
+    game.play_game(players.minimax_player, players.random_player)
     return json.dumps(game.history)
 
 
@@ -30,7 +32,7 @@ class Game:
         try:
             while not self.board.is_game_over(claim_draw=True):
                 if self.board.turn == WHITE:
-                    move = player_1(self.board)
+                    move = player_1(self.board, True)
                 else:
                     move = player_2(self.board)
                 self.board.push_uci(move)
@@ -44,25 +46,10 @@ class Game:
         self.history[self.moves] = board_fen
         self.moves += 1
 
-    def evaluate(self): 
-        material_sum = 0
-        weights = {'p': 100, 'n': 320, 'b': 330, 'r': 500, 'q': 900, 'k': 20000}
-        for i in range(64):
-            piece = self.board.piece_at(i)
-            if piece:
-                color_weight = 1 if piece.color else -1
-                material_sum += weights[piece.symbol().lower()] * color_weight
-        return material_sum
-
-
-
 #   when game is over, store the game history in redis with the Game id as the key, then increment Game id
 #   def cache_results(self):
 
-# TODO: Setup Minimax Vanilla
 # TODO: Setup Minimax w/ AlphaBeta Pruning
-
-
 
 if __name__ == '__main__':
     app.run()
